877a0052d320c362b33e7fb9fdcae8cb78d7c865
kovacspeter07 <kovacs.peter1@students.jedlik.eu>
2025-03-09 20:03:09 +0100

    beolvasás

---
+++ Megoldás.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace meneterendProjekt
+{
+    internal class Megoldás
+    {
+        private List<Menetrend> menetrendek = new List<Menetrend>();
+
+        public Megoldás(string állományNeve)
+        {
+            if (!File.Exists(állományNeve))
+            {
+                Console.WriteLine($"Error: File '{állományNeve}' not found.");
+                return;
+            }
+
+            string jsonContent = File.ReadAllText(állományNeve);
+
+            var rawData = JsonConvert.DeserializeObject<List<List<object>>>(jsonContent);
+
+            foreach (var sor in rawData)
+            {
+                string sorStr = string.Join(";", sor); 
+                menetrendek.Add(new Menetrend(sorStr)); 
+            }
+        }
+
+    }
+}
--- Menetrend.cs
+++ Menetrend.cs
@@ -8,11 +8,22 @@ namespace meneterendProjekt
 {
     internal class Menetrend
     {
-        public string ID { get; private set; }
-        public string Óra { get; private set; }
-        public string Perc { get; private set; }
-        public string Távolság { get; private set; }
+        public int ID { get; private set; }
+        public int Óra { get; private set; }
+        public int Perc { get; private set; }
+        public int Távolság { get; private set; }
         public string Irány  { get; private set; }
+
+
+        public Menetrend(string adatsor)
+        {
+            string[] m = adatsor.Split(';');
+            ID = int.Parse(m[0]);
+            Óra = int.Parse(m[1]);
+            Perc = int.Parse(m[2]);
+            Távolság = int.Parse(m[3]);
+            Irány = m[4];
+        }
 
     }
 

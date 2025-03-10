73f23cc3091724da3868d99e90f44fae1add72a2
BoriszaZsigmondBenedek <borisza.zsigmond.benedek@students.jedlik.eu>
2025-03-10 07:53:48 +0100

    kicsengettek

--- Program.cs
+++ Program.cs
@@ -13,6 +13,10 @@ namespace meneterendProjekt
             Megoldás m = new Megoldás("adat.json");
 
             Console.WriteLine($"2. feladat\nAz állomások száma: {m.ÁllomásDb}\nA vonatok száma: {m.VonatokSzáma}");
+            //Console.WriteLine($"3. feladat\nA (z) {}. vonat a (z) {}. állomáson {} percet állt.");
+            Console.Write("4. feladat\nAdja meg egy vonat azonosítóját! ");
+            int bekertvonat = int.Parse(Console.ReadLine());
+            Console.Write("Adjon meg egy időpontot (óra perc) !");
         }
     }
 }
